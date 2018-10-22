2e91fb511c1f0730db8b0286509c493e0ba3dd47
Atli Gislason <atlig17@ru.is>
2018-10-22 22:57:15 +0000

    small comment in main

--- Merlyn.cpp
+++ Merlyn.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "Entity.h"
 
 int main(int argc, const char* argv[]) {
     if(argc > 1){
@@ -6,8 +7,10 @@ int main(int argc, const char* argv[]) {
         return 0;
     }
     if(argc == 1){
-        
+
     }
-    
     return 0;
 }
+
+//Im thinking of having the whole program have a bunch of nodes that have multiple pointers.
+//It will work out to be a tree of some sorts visually when im done
