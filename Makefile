3aef2631c6134ef92b5d88d8d3f86fb4aa284ff3
Atli Gislason <atlig17@ru.is>
2018-10-22 22:14:21 +0000

    initializing structure and empty files

---
+++ Makefile
@@ -0,0 +1,2 @@
+default:
+    g++ -o merlyn Merlyn.cpp
---
+++ Merlyn.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, const char* argv[]) {
+    if(argc > 1){
+        std::cout << "Do not enter more than one argument";
+        return 0;
+    }
+    if(argc == 1){
+        
+    }
+    
+    return 0;
+}
